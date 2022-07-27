9733fd4ecc9c0d98055e2b095f88baa5c9189521
Anrey-OG <wwwfox11@mail.ru>
2022-07-27 21:50:46 +0500

    Добавил CRUD для Вопросов в приложении и страницы добавления, редактирования вопроса

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :questions
+
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # post '/questions', to: 'questions#create'
+  # get '/questions/:id/edit', to: 'questions#edit'
 
   root 'pages#index'
-  
-  get '/questions', to: 'questions#index'
 end
