f608b6d8b97f09710c61ab15ae3fef1fa7c56712
Anrey-OG <wwwfox11@mail.ru>
2022-07-28 14:01:39 +0500

    Добавил фидбек пользователю о неудачном создании вопроса или об успешном создании, редактировании, удалении вопроса. А также вывод 404 страницы при запросе несуществующего вопроса по id.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[destroy edit update show]
   def index
     @questions = Question.all
   end
@@ -10,6 +11,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new question_params
     if @question.save
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :new
@@ -17,18 +19,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by id: params[:id]
     @question.destroy
+    flash[:success] = "Question deleted!"
     redirect_to questions_path
   end
 
   def edit
-    @question = Question.find_by id: params[:id]
   end
 
   def update
-    @question = Question.find_by id: params[:id]
     if @question.update question_params
+      flash[:success] = "Question updated!"
       redirect_to questions_path
     else
       render :edit
@@ -36,12 +37,15 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find_by id: params[:id]
   end
 
   private
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def set_question
+    @question = Question.find params[:id]
   end
 end
